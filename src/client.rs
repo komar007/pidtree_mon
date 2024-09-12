4e6ca15482304f591961b9a2a95c15141dce180c
Michal Trybus <komar007@gmail.com>
2024-09-12 21:38:35 +0200

    feat: extended output format specification
    - added --separator,
    - added scaling output down by number of cores,
    - added if_range,
    - more flexible field specifier format.

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,6 @@
 use std::{fmt::Display, time::Duration};
 
 use futures::{stream::unfold, StreamExt as _};
-use itertools::Itertools as _;
 use tokio::{
     io::{AsyncReadExt as _, AsyncWriteExt as _},
     net::UnixStream as TokioUnixStream,
@@ -9,7 +8,7 @@ use tokio::{
     time::Instant,
 };
 
-use crate::config::Field;
+use crate::config::{Field, Format, Scale, Source};
 
 /// Run the client for as long as configured.
 pub async fn run(
@@ -17,6 +16,7 @@ pub async fn run(
     pids: Vec<i32>,
     timeout: Option<Duration>,
     fields: Vec<Field>,
+    separator: String,
 ) -> Result<(), String> {
     for pid in &pids {
         stream
@@ -39,7 +39,10 @@ pub async fn run(
     pin!(loads_stream);
     let deadline = timeout.map(|tmout| Instant::now() + tmout);
     while let Some(loads) = loads_stream.next().await {
-        println!("{}", OutputLine(&fields, loads));
+        println!(
+            "{}",
+            OutputLine(&fields, &separator, num_cpus::get(), loads)
+        );
         if deadline.is_some_and(|d| Instant::now() > d) {
             break;
         }
@@ -47,37 +50,92 @@ pub async fn run(
     Ok(())
 }
 
-struct OutputLine<'f>(&'f Vec<Field>, Vec<f32>);
+struct OutputLine<'a>(&'a Vec<Field>, &'a str, usize, Vec<f32>);
 
 impl<'f> Display for OutputLine<'f> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let OutputLine(spec, loads) = self;
+        let OutputLine(spec, sep, num_cores, loads) = self;
         let sum: f32 = loads
             .iter()
             .map(|l| if l.is_nan() { 0.0 } else { *l })
             .sum();
         let mut any_written = false;
-        for field in spec.iter() {
-            if any_written {
-                write!(f, " ")?;
-            }
-            match field {
-                Field::Sum => write!(f, "{sum}")?,
-                Field::AllLoads => write!(f, "{}", loads.iter().join(" "))?,
-                Field::IfGreater {
-                    value,
-                    then,
-                    otherwise,
-                } => {
-                    if sum > *value {
-                        write!(f, "{}", then)?
-                    } else {
-                        write!(f, "{}", otherwise)?
+        for Field(source, scale, format) in spec.iter() {
+            let scale = match scale {
+                Scale::OfCore => 1.0,
+                Scale::OfTotal => *num_cores as f32,
+            };
+            let inputs = match source {
+                Source::Sum => &vec![sum],
+                Source::AllLoads => loads,
+            };
+            let inputs: Vec<f32> = inputs.iter().map(|i| i / scale).collect();
+            for input in inputs {
+                if any_written {
+                    write!(f, "{sep}")?;
+                }
+                match format {
+                    Format::Float(precision) | Format::Percent(precision) => {
+                        let mul = match format {
+                            Format::Float(_) => 1.0,
+                            Format::Percent(_) => 100.0,
+                            _ => panic!(),
+                        };
+                        write!(f, "{:.1$}", input * mul, *precision as usize)?
+                    }
+                    Format::IfThenElse {
+                        test,
+                        then,
+                        otherwise,
+                    } => {
+                        if test.matches(input) {
+                            write!(f, "{}", then)?
+                        } else {
+                            write!(f, "{}", otherwise)?
+                        }
                     }
                 }
+                any_written = true;
             }
-            any_written = true;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::config::Test;
+
+    use super::*;
+
+    #[test]
+    fn test() {
+        let fields = vec![
+            Field(
+                Source::Sum,
+                Scale::OfCore,
+                Format::IfThenElse {
+                    test: Test::Range(Some(1.0), None),
+                    then: "x".to_owned(),
+                    otherwise: "y".to_owned(),
+                },
+            ),
+            Field(
+                Source::AllLoads,
+                Scale::OfCore,
+                Format::IfThenElse {
+                    test: Test::Range(None, Some(1.0)),
+                    then: "x".to_owned(),
+                    otherwise: "y".to_owned(),
+                },
+            ),
+            Field(Source::Sum, Scale::OfTotal, Format::Float(3)),
+        ];
+        let o = OutputLine(&fields, " ", 3, vec![0.5, 2.0, 3.5]);
+        assert_eq!(o.to_string(), "x x y y 2.000");
+        let o = OutputLine(&fields, "", 3, vec![0.0, 0.0, 1.5]);
+        assert_eq!(o.to_string(), "xxxy0.500");
+        let o = OutputLine(&fields, "xxx", 3, vec![]);
+        assert_eq!(o.to_string(), "yxxx0.000");
     }
 }
